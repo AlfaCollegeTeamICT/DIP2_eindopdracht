3b64ff2419e7fd03568ff896e1aa60bfc2785fd3
Wouter Weges <wg.weges@alfa-college.nl>
2024-10-21 11:55:51 +0200

    Eerste spul

---
+++ Alfaflix.md
@@ -0,0 +1,46 @@
+# Alfaflix
+
+Eindopdracht DIP 2.
+
+Applicatie verbeteren en uitbreiden
+
+In de code letterlijke opdrachten zetten zoals ze op het examen ook kunnen verwachten
+
+Applicatie moet een lening beheer systeem zijn voor een fysieke winkel die dvd’s verhuurt.
+
+Er word bij gehouden wat er in voorraad is en wat gereserveerd is
+
+### Vereisten
+
+- Stroomschema maken van 1 van de nieuwe ontwerpen
+- Inlog systeem om te kunnen lenen
+- Logging van leningen
+- Beschikbaarheid van dvd’s bijhouden
+    - uitgeleend/niet uitgeleend.
+    - als uitgeleend niet uit te lenen
+- CRUD voor DVD’s implementeren
+    - C en D alleen via API
+    - R en U via API en Website
+- Bootstrap
+- Gebruik van base.html en extenden op andere pagina’s
+- Swagger documentatie
+
+### Keuze opdracht 1
+
+- Reserveren van dvd
+- Alleen als ingelogd
+- DVD niet beschikbaar markeren
+
+### Keuze opdracht 2
+
+- Externe API gebruiken
+- The Movie DataBase
+    - Plaatjes importeren en beschrijvingen van de films
+
+### Bonus opties
+
+- rollen implementatie
+    - Admin met admin paneel om dvd’s toe te voegen
+    - gebruiker rol
+- 2FA
+- OAuth authenticatie
---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SECRET_KEY = os.urandom(32)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'videotheek.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+CORS(app)
+
+db = SQLAlchemy(app)
+
+class Film(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+
+# !! Maak een class voor de gebruikers aan zodat er geregistreert en ingelogt kan worden zodat er een tabel beschikbaar is in de database
+
+class Loggin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    film_id = db.Column(db.Integer, db.ForeignKey('film.id'), nullable=False)
+    renttimestamp = db.Column(db.DateTime, nullable=False, default = datetime.now())
+    returntimestamp = db.Column(db.DateTime, nullable=True)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/videotheek')
+def videotheek():
+    return render_template('videotheek.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
